0632ecc40088ccece4964540cd7f5889d8b8a5e8
Anis Agwan <anis.agwan@wsisrdev.net>
2021-12-30 21:41:58 +0530

    Initial Commit

---
+++ Goals/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Goals
+//
+//  Created by Anis Agwan on 30/12/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Goals/GoalsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoalsApp.swift
+//  Goals
+//
+//  Created by Anis Agwan on 30/12/21.
+//
+
+import SwiftUI
+
+@main
+struct GoalsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
