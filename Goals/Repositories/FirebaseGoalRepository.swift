10a3e483fb0d9270ce05c5ad925df5c0cd2e4ab7
Anis Agwan <anis.agwan@wsisrdev.net>
2022-01-01 16:20:56 +0530

    Added the color utils

--- Goals/Repositories/FirebaseGoalRepository.swift
+++ Goals/Repositories/FirebaseGoalRepository.swift
@@ -9,7 +9,9 @@ import Foundation
 import Firebase
 import FirebaseFirestoreSwift
 
-class FirebaseGoalRepository {
+class FirebaseGoalRepository: GoalRepositoryProtocol {
+    
+    
     private let db = Firestore.firestore()
     
     func add(goal: Goal, completion: @escaping(Result<Goal?, Error>) -> Void) {
@@ -28,6 +30,83 @@ class FirebaseGoalRepository {
             
         } catch let error {
             completion(.failure(error))
+        }
+    }
+    
+    
+    func getAll(completion: @escaping (Result<[Goal]?, Error>) -> Void) {
+        db.collection("Goals").getDocuments { snapshot, error in
+            guard let snapshot = snapshot, error == nil else {
+                completion(.failure(error ?? NSError(domain: "Snapshot get all documents is nil", code: 102, userInfo: nil)))
+                return
+            }
+            
+            let goals: [Goal] = snapshot.documents.compactMap { doc in
+                var goal = try? doc.data(as: Goal.self)
+                if goal != nil {
+                    goal!.id = doc.documentID
+                }
+                return goal
+            }
+            
+            completion(.success(goals))
+        }
+    }
+    
+    func deleteGoal(goalId: String, completion: @escaping (Result<Bool, Error>) -> Void) {
+        db.collection("Goals").document(goalId).delete { error in
+            if let error = error {
+                completion(.failure(error))
+            } else {
+                completion(.success(true))
+            }
+        }
+    }
+    
+    func addGoalItem(goalId: String, item: String, completion: @escaping (Result<Goal?, Error>) -> Void) {
+        let ref = db.collection("Goals").document(goalId)
+        
+        ref.updateData(["items": FieldValue.arrayUnion([item])]) { error in
+            if let error = error {
+                completion(.failure(error))
+            } else {
+                ref.getDocument { snapshot, error in
+                    guard let snapshot = snapshot, error == nil else {
+                        completion(.failure(error ?? NSError(domain: "Snapshot item update document is nil", code: 103, userInfo: nil)))
+                        return
+                    }
+                    
+                    var goal: Goal? = try? snapshot.data(as: Goal.self)
+                    if goal != nil {
+                        goal!.id = snapshot.documentID
+                        completion(.success(goal))
+                    }
+                    
+                }
+            }
+        }
+    }
+    
+    func deleteItem(goalId: String, item: String, completion: @escaping (Result<Goal?, Error>) -> Void) {
+        let ref = db.collection("Goals").document(goalId)
+        
+        ref.updateData(["items": FieldValue.arrayRemove([item])]) { error in
+            if let error = error {
+                completion(.failure(error))
+            } else {
+                ref.getDocument { snapshot, error in
+                    guard let snapshot = snapshot, error == nil else {
+                        completion(.failure(error ?? NSError(domain: "Snapshot item update document is nil", code: 103, userInfo: nil)))
+                        return
+                    }
+                    
+                    var goal: Goal? = try? snapshot.data(as: Goal.self)
+                    if goal != nil {
+                        goal!.id = snapshot.documentID
+                        completion(.success(goal))
+                    }
+                }
+            }
         }
     }
 }
---
+++ Goals/Utils/ColorUtils.swift
@@ -0,0 +1,48 @@
+//
+//  ColorUtils.swift
+//  Goals
+//
+//  Created by Anis Agwan on 01/01/22.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    convenience init(hexString: String, alpha: CGFloat = 1.0) {
+        let hexString: String = hexString.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
+        let scanner = Scanner(string: hexString)
+        if hexString.hasPrefix("#") {
+            //scanner.currentIndex = scanner.string.index(after: scanner.currentIndex)
+            scanner.scanLocation = 1
+        }
+        
+        var color: UInt64 =  0
+        scanner.scanHexInt64(&color)
+        let mask = 0x000000FF
+        let r = Int(color >> 16) & mask
+        let g = Int(color >> 8) & mask
+        let b = Int(color) & mask
+        
+        let red = CGFloat(r) / 255.0
+        let green = CGFloat(g) / 255.0
+        let blue = CGFloat(b) / 255.0
+        
+        self.init(red: red, green: green, blue: blue, alpha: alpha)
+        
+    }
+    
+    func hexStringFromColor() -> String {
+        var r: CGFloat = 0
+        var g: CGFloat = 0
+        var b: CGFloat = 0
+        var a: CGFloat = 0
+        
+        getRed(&r, green: &g, blue: &b, alpha: &a)
+        
+        let rgb: Int = (Int)(r * 255.0) << 16 | (Int)(g * 255.0) << 8 | (Int)(b * 255.0) << 0
+        
+        return String(format: "#%06x", rgb)
+    }
+    
+}
