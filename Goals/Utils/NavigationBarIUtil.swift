e27eaeeae67a923f24a74da73263b3d034b4b0ea
Anis Agwan <anis.agwan@wsisrdev.net>
2022-01-01 21:10:43 +0530

    Created the NavigationBar utils

---
+++ Goals/Utils/NavigationBarIUtil.swift
@@ -0,0 +1,28 @@
+//
+//  NavigationBarIUtil.swift
+//  Goals
+//
+//  Created by Anis Agwan on 01/01/22.
+//
+
+import SwiftUI
+import UIKit
+
+class Theme {
+    static func navigationBarColors(background: Color?, titleColor: Color? = nil, tintColor: Color? = nil) {
+        let navigationAppearance = UINavigationBarAppearance()
+        navigationAppearance.configureWithOpaqueBackground()
+        navigationAppearance.backgroundColor = UIColor(background ?? Color.clear)
+        
+        navigationAppearance.shadowColor = .clear
+        
+        navigationAppearance.titleTextAttributes = [.foregroundColor: UIColor(titleColor ?? .black)]
+        navigationAppearance.largeTitleTextAttributes = [.foregroundColor: UIColor(titleColor ?? .black)]
+        
+        UINavigationBar.appearance().standardAppearance = navigationAppearance
+        UINavigationBar.appearance().compactAppearance = navigationAppearance
+        UINavigationBar.appearance().scrollEdgeAppearance = navigationAppearance
+        
+        UINavigationBar.appearance().tintColor = UIColor(tintColor ?? titleColor ?? .black)
+    }
+}
