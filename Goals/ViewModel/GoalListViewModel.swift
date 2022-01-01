ab2cb88ee506a47d49820762c0ae081580ae16fd
Anis Agwan <anis.agwan@wsisrdev.net>
2022-01-01 21:40:19 +0530

    Created the GoalListViewModel to fetch all goals and delete goal to convert goals into goalviewmodel

---
+++ Goals/ViewModel/GoalListViewModel.swift
@@ -0,0 +1,46 @@
+//
+//  GoalListViewModel.swift
+//  Goals
+//
+//  Created by Anis Agwan on 01/01/22.
+//
+
+import Foundation
+
+class GoalListViewModel: ObservableObject {
+    private var repo: GoalRepositoryProtocol
+    
+    @Published var goals = [GoalViewModel]()
+    
+    init(repo: GoalRepositoryProtocol) {
+        self.repo = repo
+    }
+    
+    func getAllGoals() {
+        repo.getAll { result in
+            switch result {
+            case .success(let fetchedGoals):
+                if let fetchedGoals = fetchedGoals {
+                    DispatchQueue.main.async {
+                        self.goals = fetchedGoals.map(GoalViewModel.init)
+                    }
+                }
+            case .failure(let err):
+                print(err.localizedDescription)
+            }
+        }
+    }
+    
+    func deleteGoal(goalId: String) {
+        repo.deleteGoal(goalId: goalId) { result in
+            switch result {
+            case .success(let success):
+                if success {
+                    self.getAllGoals()
+                }
+            case .failure(let err):
+                print(err.localizedDescription)
+            }
+        }
+    }
+}
